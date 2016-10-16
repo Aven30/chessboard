81efb86d8cf2aa651c7f36f9608af78b2b3a8a36
Maruti-Ram <maruti.ram848@gmail.com>
2016-10-15 17:24:06 -0700

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -1,13 +1,19 @@
+import treelib
 def minimax_value(node, depth, maximizing):
     if depth <= 0 or node.is_terminal:
         return node.score
     if maximizing:
         f = max
         bestvalue = float('-inf')
+		for each child of node
+            val := minimax(child, depth − 1, False)
+            bestValue = f(bestvalue, val)
+        return bestValue
     else:
         f = min
         bestvalue = float('inf')
     for child in node.children:
-        val = minimax_value(child, depth - 1, not maximizing)
-        bestvalue = f(bestvalue, val)
+        val = minimax_value(child, depth - 1, True)
+        bestValue = f(bestvalue, val)
     return bestvalue
+
